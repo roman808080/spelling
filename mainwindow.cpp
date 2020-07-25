91d1344a967165bc3dec0007de16f1eeb9460e29
Roman Herasymenko <roman.herasymenko@gmail.com>
2020-07-25 20:33:30 +0300

    Added getting information from json.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,15 +2,35 @@
 #include "ui_mainwindow.h"
 
 #include <QString>
+#include <QDir>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    auto currentPath = QDir::currentPath();
+    QDir jsonPath(currentPath + QDir::separator() +
+                  "modules" + QDir::separator() +
+                  "default" + QDir::separator() +
+                  "info.json");
+
+    QFile file(jsonPath.path());
+    file.open(QIODevice::ReadOnly | QIODevice::Text);
+    auto bytes = file.readAll();
+    file.close();
+
+    auto jsonDocument = QJsonDocument::fromJson(bytes);
+    auto jsonObject = jsonDocument.object();
+
     ui->setupUi(this);
-    for (int i=0; i<100; ++i)
+
+    ui->lessons->clear();
+    for (const auto& key: jsonObject.keys())
     {
-        ui->lessons->addItem(QString::number(i));
+        ui->lessons->addItem(key);
     }
 }
 
