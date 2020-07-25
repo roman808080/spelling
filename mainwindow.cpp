da46c17dc1c032b3806ea21c5728bdc766e9b796
Roman Herasymenko <roman.herasymenko@gmail.com>
2020-07-25 18:19:24 +0300

    Added widgets.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QString>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    for (int i=0; i<100; ++i)
+    {
+        ui->lessons->addItem(QString::number(i));
+    }
 }
 
 MainWindow::~MainWindow()
 {
 }
-
